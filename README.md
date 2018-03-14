c8bc2fb9bbde158e795ad5ae310453ce9bc80cff
solbach <solbach.m@gmail.com>
2018-03-14 11:30:58 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# polyhedral_api
-Example API script for the polyhedral.eecs.yorku.ca project
+# Polyhedral API
+
+This is the home of the "Random Polyhedral Scenes: An Image Generator for Active Vision System Experiments" API ([polyhedral.eecs.yorku.ca](http://polyhedral.eecs.yorku.ca)). 
+The API is realized using WebSockets. Basically any programming language that supports WebSockets can be used.
+The example provided is written in Python 3 and can be found [HERE](api_example.py)
+
+## Requirements for example script
+* ```websocket```
+* ```json```
+* ```base64```
+* ```io```
+* ```sys```
+
+## Optional for example script
+* ```PIL```
---
+++ api_example.py
@@ -0,0 +1,52 @@
+# Author: Markus Solbach (polyhedral@eecs.yorku.ca)
+from websocket import create_connection
+import io, sys, json, base64
+from json import dumps
+try:
+    from PIL import Image
+except ImportError:
+    print("PIL not installed on system. Running lightweight example.")
+
+# Create Connection
+ws = create_connection("ws://polyhedral.eecs.yorku.ca/api/")
+
+# Mandatory
+parameter = {
+    'ID':'782fec97-83c0-42d2-b09b-7d3fe37b2317b',
+    'light_fixed':'true',
+    'random_cam': 'true',
+    'cam_x':-0.911,
+    'cam_y':1.238,
+    'cam_z':-4.1961,
+    'cam_qw':-0.0544,
+    'cam_qx':-0.307,
+    'cam_qy':0.9355,
+    'cam_qz':0.16599
+}
+
+json_params = dumps(parameter, indent=2)
+ws.send(json_params)
+
+while True:
+    result = json.loads(ws.recv())
+    print("Job Status: {0}".format(result['status']))
+    if result['status'] == "SUCCESS":
+        break
+    elif "FAILURE" in result['status'] or "INVALID" in result['status']:
+        sys.exit()
+
+image_base64 = result['image']
+image_decoded = base64.b64decode(image_base64)
+random_cam_param = result['cam_pose']
+print(random_cam_param)
+
+fh = open("imageToSave.png", "wb")
+fh.write(image_decoded)
+fh.close()
+
+if 'PIL' in sys.modules:
+    im = Image.open(io.BytesIO(image_decoded))
+    im.show()
+
+# Close Connection
+ws.close()
