fe53dccd71683201af77a29fb0dc36c0163c9557
Markus Solbach <solbach@cse.yorku.ca>
2021-03-13 12:18:30 -0500

    updated example for use with 2nd machine :star:

--- Python/api_example.py
+++ Python/api_example.py
@@ -7,6 +7,7 @@ import numpy as np
 
 # Create Connection
 ws = create_connection("wss://polyhedral.eecs.yorku.ca/api/")
+ws = create_connection("ws://230t.eecs.yorku.ca:8044/api/") # only available to students at YorkU (note: ws vs wss for the protocol)
 
 # Set Parameters
 parameter = {
